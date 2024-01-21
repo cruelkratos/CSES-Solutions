edcddc41e72f1eaf8c2a330b3c76500995036a2b
[REDACTED] <116339436+cruelkratos@users.noreply.github.com>
2024-01-21 16:24:43 +0530

    Update 12_Collecting_Numbers2.cpp

--- Sorting Searching/12_Collecting_Numbers2.cpp
+++ Sorting Searching/12_Collecting_Numbers2.cpp
@@ -15,70 +15,70 @@ typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_upda
  
  
 void solve(){
-	ll int n,m;
-	cin>>n>>m;
-	vll a(n+1);
-	a[0]=0;
-	vll v(n);
-	for(int i = 0;i<n;i++){
-		cin>>v[i];
-		a[v[i]] = i;
-	}
-	ll ans = 1;
-	unordered_set<ll> s;
-	for(int i = 1;i<=n;i++){
-		if(a[i]<a[i-1]){
-			ans++;
-			s.insert(i);
-		}
-	}
-	
-	a.push_back(MOD);
-	for(int i = 0;i<m;i++){
-		ll int x,y;
-		cin>>x>>y;
-		if(s.find(v[x-1] +1) !=s.end()) {ans--;
-		s.erase(v[x-1]+1);
-		}
-		if(s.find(v[x-1]) !=s.end()) {ans--;
-		s.erase(v[x-1]);
-		}
-		if(s.find(v[y-1] +1) !=s.end()) {ans--;
-		s.erase(v[y-1]+1);
-		}
-		if(s.find(v[y-1]) !=s.end()) {ans--;
-		s.erase(v[y-1]);
-		}
-		
-		swap(a[v[x-1]] , a[v[y-1]]);
-		swap(v[x-1],v[y-1]);
-		if(a[v[x-1]] <a[v[x-1] -1] ){
-			ans++;
-			s.insert(v[x-1]);
-		}
-		if(a[v[x-1]] > a[v[x-1] +1] ){
-			ans++;
-			s.insert(v[x-1] +1);
-		}
-		if(a[v[y-1]] <a[v[y-1] -1] && s.find(v[y-1])==s.end()){
-			ans++;
-			s.insert(v[y-1]);
-		}
-		if(a[v[y-1]] > a[v[y-1] +1] && s.find(v[y-1] +1)==s.end()){
-			ans++;
-			s.insert(v[y-1] +1);
-		}
-		cout<<ans<<endl;
-	}
-	 
-	
+  ll int n,m;
+  cin>>n>>m;
+  vll a(n+1);
+  a[0]=0;
+  vll v(n);
+  for(int i = 0;i<n;i++){
+    cin>>v[i];
+    a[v[i]] = i;
+  }
+  ll ans = 1;
+  unordered_set<ll> s;
+  for(int i = 1;i<=n;i++){
+    if(a[i]<a[i-1]){
+      ans++;
+      s.insert(i);
+    }
+  }
+  
+  a.push_back(MOD);
+  for(int i = 0;i<m;i++){
+    ll int x,y;
+    cin>>x>>y;
+    if(s.find(v[x-1] +1) !=s.end()) {ans--;
+    s.erase(v[x-1]+1);
+    }
+    if(s.find(v[x-1]) !=s.end()) {ans--;
+    s.erase(v[x-1]);
+    }
+    if(s.find(v[y-1] +1) !=s.end()) {ans--;
+    s.erase(v[y-1]+1);
+    }
+    if(s.find(v[y-1]) !=s.end()) {ans--;
+    s.erase(v[y-1]);
+    }
+    
+    swap(a[v[x-1]] , a[v[y-1]]);
+    swap(v[x-1],v[y-1]);
+    if(a[v[x-1]] <a[v[x-1] -1] ){
+      ans++;
+      s.insert(v[x-1]);
+    }
+    if(a[v[x-1]] > a[v[x-1] +1] ){
+      ans++;
+      s.insert(v[x-1] +1);
+    }
+    if(a[v[y-1]] <a[v[y-1] -1] && s.find(v[y-1])==s.end()){
+      ans++;
+      s.insert(v[y-1]);
+    }
+    if(a[v[y-1]] > a[v[y-1] +1] && s.find(v[y-1] +1)==s.end()){
+      ans++;
+      s.insert(v[y-1] +1);
+    }
+    cout<<ans<<endl;
+  }
+   
+  
 }
  
 int main(){
-	
-	fastfast
-	ll int tt=1;
-	// cin>>tt;
-	while(tt--)
-		solve();
-	return 0;}
+  
+  fastfast
+  ll int tt=1;
+  // cin>>tt;
+  while(tt--)
+    solve();
+  return 0;}
