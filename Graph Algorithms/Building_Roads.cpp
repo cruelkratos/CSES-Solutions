1fa823e4d73d547101ef371e2a97112f6b060165
[REDACTED] <116339436+cruelkratos@users.noreply.github.com>
2024-03-27 10:39:19 +0530

    Update Building_Roads.cpp

--- Graph Algorithms/Building_Roads.cpp
+++ Graph Algorithms/Building_Roads.cpp
@@ -8,43 +8,43 @@
 using namespace std;
  
 void dfs(vector<vector<int>> &adj,vector<bool> &vis,int v){
-	for(auto u: adj[v]){
-		if(!vis[u]){
-			vis[u]= true;
-			dfs(adj,vis,u);
-		}
-	}
+  for(auto u: adj[v]){
+    if(!vis[u]){
+      vis[u]= true;
+      dfs(adj,vis,u);
+    }
+  }
 }
  
 void solve(){
-	ll int n,m;
-	cin>>n>>m;
-	vector<vector<int>> adj(n+1);
-	for(int i = 0;i<m;i++){
-		int x,y;cin>>x>>y;
-		adj[x].push_back(y);
-		adj[y].push_back(x);
-	}
-	vector<bool> vis(n+1,false);
-	vector<int> road;
-	for(int i = 1;i<=n;i++){
-		if(!vis[i]){
-			road.push_back(i);
-			vis[i] = true;
-			dfs(adj,vis,i);
-		}
-	}
-	cout<<road.size()-1<<endl;
-	for(int i = 0;i<road.size()-1;i++){
-		cout<<road[i]<<" "<<road[i+1]<<endl;
-	}
+  ll int n,m;
+  cin>>n>>m;
+  vector<vector<int>> adj(n+1);
+  for(int i = 0;i<m;i++){
+    int x,y;cin>>x>>y;
+    adj[x].push_back(y);
+    adj[y].push_back(x);
+  }
+  vector<bool> vis(n+1,false);
+  vector<int> road;
+  for(int i = 1;i<=n;i++){
+    if(!vis[i]){
+      road.push_back(i);
+      vis[i] = true;
+      dfs(adj,vis,i);
+    }
+  }
+  cout<<road.size()-1<<endl;
+  for(int i = 0;i<road.size()-1;i++){
+    cout<<road[i]<<" "<<road[i+1]<<endl;
+  }
 }
  
 int main(){
-	
-	fastfast
-	ll int tt=1;
-	// cin>>tt;
-	while(tt--)
-		solve();
-	return 0;}
+  
+  fastfast
+  ll int tt=1;
+  // cin>>tt;
+  while(tt--)
+    solve();
+  return 0;}
